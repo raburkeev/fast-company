327fcabf452e06d0e8226ed071553ec645155251
Artur Zhitkovich <artur.zhitkovich15041998@gmail.com>
2022-07-15 15:08:15 +0300

    Update bookmark.jsx

--- src/components/bookmark.jsx
+++ src/components/bookmark.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 const BookMark = ({onToggleBookMark, userId, user}) => {
-    const marked = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+    const marked = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" // вынеси из компонента, и вообще, это плохой вариант
                         className="bi bi-bookmark-fill" viewBox="0 0 16 16">
         <path
             d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2z"/>
