b303fd2cd9e3c6dd388b59049db82f3f07f34efa
Roman Burkeev <raburkeev@gmail.com>
2022-07-27 01:27:28 +0300

    deleted check on array from groupList

--- src/components/groupList.jsx
+++ src/components/groupList.jsx
@@ -8,47 +8,24 @@ const GroupList = ({
     onItemSelect,
     selectedItem
 }) => {
-    return Array.isArray(items)
-        ? (
-            <>
-                <ul className="list-group">
-                    {items.map((item) => {
-                        return (
-                            <li
-                                key={item[valueProperty]}
-                                className={`list-group-item ${
-                                    item === selectedItem ? 'active' : ''
-                                }`}
-                                role="button"
-                                onClick={() => onItemSelect(item)}
-                            >
-                                {item[contentProperty]}
-                            </li>
-                        )
-                    })}
-                </ul>
-            </>
-        )
-        : (
-            <>
-                <ul className="list-group">
-                    {Object.keys(items).map((item) => {
-                        return (
-                            <li
-                                key={items[item][valueProperty]}
-                                className={`list-group-item ${
-                                    items[item] === selectedItem ? 'active' : ''
-                                }`}
-                                role="button"
-                                onClick={() => onItemSelect(items[item])}
-                            >
-                                {items[item][contentProperty]}
-                            </li>
-                        )
-                    })}
-                </ul>
-            </>
-        )
+    return (
+        <ul className="list-group">
+            {Object.keys(items).map((item) => {
+                return (
+                    <li
+                        key={items[item][valueProperty]}
+                        className={`list-group-item ${
+                            items[item] === selectedItem ? 'active' : ''
+                        }`}
+                        role="button"
+                        onClick={() => onItemSelect(items[item])}
+                    >
+                        {items[item][contentProperty]}
+                    </li>
+                )
+            })}
+        </ul>
+    )
 }
 
 GroupList.defaultProps = {
