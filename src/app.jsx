cb8cddd2d4a2071e71709c89c7053e6e74c4c109
Roman Burkeev <raburkeev@gmail.com>
2022-08-03 00:26:13 +0300

    made Table reusable

--- src/app.jsx
+++ src/app.jsx
@@ -1,43 +1,8 @@
-import React, { useState, useEffect } from 'react'
-import api from './api'
+import React from 'react'
 import Users from './components/users'
-import Loader from './components/loader'
 
 const App = () => {
-    const [users, setUsers] = useState([])
-    const [isUsersLoading, setIsUsersLoading] = useState(true)
-
-    useEffect(() => {
-        api.users.fetchAll().then((data) => {
-            setIsUsersLoading(false)
-            setUsers(data)
-        })
-    }, [])
-
-    const handleDelete = (userId) => {
-        setUsers(users.filter(user => user !== userId))
-    }
-
-    const handleToggleBookMark = (userId) => {
-        const updatedState = [...users]
-        const userFoundById = updatedState.find((user) => user._id === userId)
-        userFoundById.bookmark = !userFoundById.bookmark
-        setUsers(updatedState)
-    }
-
-    return (
-        <>
-            {isUsersLoading
-                ? <Loader loadingTarget={'users'} margin={5}/>
-                : (
-                    <Users
-                        users={users}
-                        onDelete={handleDelete}
-                        onToggleBookMark={handleToggleBookMark}
-                    />
-                )}
-        </>
-    )
+    return <Users/>
 }
 
 export default App
