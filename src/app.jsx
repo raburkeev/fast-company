5e7aa21529dd10155c7222c768947e6d46896256
Roman Burkeev <raburkeev@gmail.com>
2022-08-10 22:30:48 +0300

    add navBar and routes

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import Users from './components/users'
+import NavBar from './components/navBar'
+import { Switch, Route } from 'react-router-dom'
+import Main from './components/main'
+import Login from './components/login'
 
 const App = () => {
-    return <Users/>
+    return (
+        <>
+            <NavBar />
+            <Switch>
+                <Route path="/main" component={Main} />
+                <Route path="/login" component={Login} />
+                <Route path="/users" component={Users} />
+            </Switch>
+
+        </>
+    )
 }
 
 export default App
