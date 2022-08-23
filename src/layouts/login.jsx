efd7621d5b6b60a8406dcb53aac762bc5769e94a
Roman Burkeev <raburkeev@gmail.com>
2022-08-23 21:54:21 +0300

    loginForm and textField component with validation

--- src/layouts/login.jsx
+++ src/layouts/login.jsx
@@ -1,10 +1,82 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import TextField from '../components/textField'
+import { validator } from '../utils/validator'
 
 const Login = () => {
+    const [data, setData] = useState({
+        email: '',
+        password: ''
+    })
+    const [errors, setErrors] = useState({})
+
+    const validatorConfig = {
+        email: {
+            isRequired: { message: 'Электронная почта обязательна для заполнения' },
+            isEmail: { message: 'Email введен некорректно' }
+        },
+        password: {
+            isRequired: { message: 'Пароль обязателен для заполнения' },
+            isCapitalSymbol: { message: 'Пароль должен содержать хотя бы одну заглавную букву' },
+            isContainDigit: { message: 'Пароль должен содержать хотя бы одну цифру' },
+            minLength: { message: 'Пароль должен быть не менее 8 символов', value: 8 }
+        }
+    }
+
+    useEffect(() => {
+        validate()
+    }, [data])
+
+    const validate = () => {
+        const errors = validator(data, validatorConfig)
+        setErrors(errors)
+        return Object.keys(errors).length === 0
+    }
+
+    const isValid = Object.keys(errors).length === 0
+
+    const handleChange = (event) => {
+        const { target } = event
+        setData((prevState) => ({
+            ...prevState,
+            [target.name]: target.value
+        }))
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const isValid = validate()
+
+        if (!isValid) return
+
+        console.log(data)
+    }
+
     return (
-        <h1>
-            Login
-        </h1>
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-md-6 offset-md-3 p-4 shadow">
+                    <h3 className="mb-4">Login</h3>
+                    <form onSubmit={handleSubmit}>
+                        <TextField
+                            label="Электронная почта"
+                            name="email"
+                            value={data.email}
+                            onChange={handleChange}
+                            error={errors.email}
+                        />
+                        <TextField
+                            label="Пароль"
+                            type="password"
+                            name="password"
+                            value={data.password}
+                            onChange={handleChange}
+                            error={errors.password}
+                        />
+                        <button className="btn btn-primary w-100 mx-auto" type="submit" disabled={!isValid}>Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 
