eea4998637ffc31a494f0ac27fe592906b5e2c47
Roman Burkeev <fneka17@gmail.com>
2022-10-13 16:35:38 +0300

    add proptectedRoute component

---
+++ src/app/components/common/protectedRoute.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {Route, Redirect} from 'react-router-dom'
+import {useAuth} from '../../hooks/useAuth'
+
+const ProtectedRoute = ({component: Component, children, ...rest}) => {
+    const {currentUser} = useAuth()
+    return (
+        <Route {...rest} render={(props) => {
+            if (!currentUser) {
+                return <Redirect to="/login" />
+            }
+            return Component ? <Component {...props} /> : children
+        }} />
+    )
+}
+
+ProtectedRoute.propTypes = {
+    children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
+    component: PropTypes.func
+}
+
+export default ProtectedRoute
