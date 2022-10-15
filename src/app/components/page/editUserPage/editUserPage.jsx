a28966c2991bba2aea4a636547726707452991c1
Roman Burkeev <raburkeev@gmail.com>
2022-10-15 20:27:34 +0300

    refactor editUserPage with custom hooks

--- src/app/components/page/editUserPage/editUserPage.jsx
+++ src/app/components/page/editUserPage/editUserPage.jsx
@@ -1,10 +1,7 @@
-/*eslint-disable*/
 import React, {useEffect, useState} from 'react'
 import {useParams, useHistory} from 'react-router-dom'
 import * as yup from 'yup'
 import TextField from '../../common/form/textField'
-import api from '../../../api'
-import Loader from '../../common/loader'
 import SelectField from '../../common/form/selectField'
 import RadioField from '../../common/form/radioField'
 import MultiSelectField from '../../common/form/multiSelectField'
@@ -12,15 +9,16 @@ import BackHistoryButton from '../../common/backHistoryButton'
 import {useProfessions} from '../../../hooks/useProfession'
 import {useQualities} from '../../../hooks/useQuality'
 import {useUsers} from '../../../hooks/useUsers'
+import {useAuth} from '../../../hooks/useAuth'
 
 const EditUserPage = () => {
     const history = useHistory()
     const {userId} = useParams()
+    const {editUserData} = useAuth()
     const {getUserById} = useUsers()
     const user = getUserById(userId)
 
-
-    const {professions, isLoading: isProfessionsLoading, getProfession} = useProfessions()
+    const {professions, isLoading: isProfessionsLoading} = useProfessions()
     const professionsList = professions.map(prof => ({
         label: prof.name,
         value: prof._id
@@ -39,52 +37,29 @@ const EditUserPage = () => {
         }
     }, [isProfessionsLoading, isQualitiesLoading])
 
+    const transformQualitiesData = (qualitiesData) => {
+        return qualitiesData.map(qual => ({label: qual.name, value: qual._id}))
+    }
+
     const [data, setData] = useState({
         name: user.name,
         email: user.email,
         profession: user.profession,
         sex: user.sex,
-        qualities: user.qualities
+        qualities: transformQualitiesData(user.qualities.map(q => getQuality(q)))
     })
-    const getDefaultQualitiesFormat = (qualities) => {
-        return qualities.map(q => getQuality(q)).map(qual => ({label: qual.name, value: qual._id}))
-    }
 
-    const getProfessionById = (id) => {
-        for (const prof of professions) {
-            if (prof.value === id) {
-                return {_id: prof.value, name: prof.label}
-            }
-        }
-    }
-
-    const getQualities = (elements) => {
-        const qualitiesArray = []
-        for (const elem of elements) {
-            for (const quality in qualities) {
-                if (elem.value === qualities[quality].value) {
-                    qualitiesArray.push({
-                        _id: qualities[quality].value,
-                        name: qualities[quality].label,
-                        color: qualities[quality].color
-                    })
-                }
-            }
-        }
-        return qualitiesArray
-    }
-
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault()
         const isValid = validate()
         if (!isValid) return
-        const {profession, qualities} = data
-        api.users.update(userId, {
+
+        await editUserData({
+            ...user,
             ...data,
-            qualities: getQualities(qualities),
-            profession: getProfessionById(profession)
-        }).then(() => history.push(`/users/${userId}`))
-        console.log(data)
+            qualities: data.qualities.map(q => q.value)
+        })
+        history.replace(`/users/${userId}`)
     }
 
     const validateSchema = yup.object().shape({
@@ -113,57 +88,57 @@ const EditUserPage = () => {
     const isValid = Object.keys(errors).length
 
     return !isDataLoading && (
-            <div className="container mt-5">
-                <BackHistoryButton/>
-                <div className="row">
-                    <div className="col-md-6 offset-md-3 p-4 shadow">
-                        <form onSubmit={handleSubmit}>
-                            <h1>Edit</h1>
-                            <TextField
-                                label="Имя:"
-                                onChange={handleChange}
-                                value={data.name}
-                                name="name"
-                                error={errors.name}
-                            />
-                            <TextField
-                                label="Электронная почта:"
-                                onChange={handleChange}
-                                value={data.email}
-                                name="email"
-                                error={errors.email}
-                            />
-                            <SelectField
-                                label="Выбери свою профессию:"
-                                value={data.profession}
-                                onChange={handleChange}
-                                defaultOption="Choose..."
-                                options={professionsList}
-                                name="profession"
-                            />
-                            <RadioField
-                                options={[
-                                    {name: 'Male', value: 'male'},
-                                    {name: 'Female', value: 'female'},
-                                    {name: 'Other', value: 'other'}
-                                ]}
-                                label="Выберите ваш пол:"
-                                value={data.sex}
-                                name="sex"
-                                onChange={handleChange}
-                            />
-                            <MultiSelectField
-                                options={qualitiesList}
-                                label="Ваши качества:"
-                                onChange={handleChange}
-                                name="qualities"
-                                defaultValue={getDefaultQualitiesFormat(data.qualities)}
-                            />
-                            <button className="btn btn-primary w-100 mx-auto" disabled={isValid}>Обновить</button>
-                        </form>
-                    </div>
+        <div className="container mt-5">
+            <BackHistoryButton/>
+            <div className="row">
+                <div className="col-md-6 offset-md-3 p-4 shadow">
+                    <form onSubmit={handleSubmit}>
+                        <h1>Edit</h1>
+                        <TextField
+                            label="Имя:"
+                            onChange={handleChange}
+                            value={data.name}
+                            name="name"
+                            error={errors.name}
+                        />
+                        <TextField
+                            label="Электронная почта:"
+                            onChange={handleChange}
+                            value={data.email}
+                            name="email"
+                            error={errors.email}
+                        />
+                        <SelectField
+                            label="Выбери свою профессию:"
+                            value={data.profession}
+                            onChange={handleChange}
+                            defaultOption="Choose..."
+                            options={professionsList}
+                            name="profession"
+                        />
+                        <RadioField
+                            options={[
+                                {name: 'Male', value: 'male'},
+                                {name: 'Female', value: 'female'},
+                                {name: 'Other', value: 'other'}
+                            ]}
+                            label="Выберите ваш пол:"
+                            value={data.sex}
+                            name="sex"
+                            onChange={handleChange}
+                        />
+                        <MultiSelectField
+                            options={qualitiesList}
+                            label="Ваши качества:"
+                            onChange={handleChange}
+                            name="qualities"
+                            defaultValue={data.qualities}
+                        />
+                        <button className="btn btn-primary w-100 mx-auto" disabled={isValid}>Обновить</button>
+                    </form>
                 </div>
             </div>
+        </div>
     )
 }
 
--- src/app/services/user.service.js
+++ src/app/services/user.service.js
@@ -17,6 +17,10 @@ const userService = {
             userEndpoint + localStorageService.getUserId()
         )
         return data
+    },
+    update: async (payload) => {
+        const {data} = await httpService.patch(`${userEndpoint}${payload._id}`, payload)
+        return data
     }
 }
 
