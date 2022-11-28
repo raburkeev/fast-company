ced85bf7f57d21f339040569b0919d4830cb11e3
Roman Burkeev <fneka17@gmail.com>
2022-11-28 22:15:59 +0300

    start server

---
+++ server/routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router({mergeParams: true})
+
+router.use('/auth', require('./auth.routes'))
+router.use('/comment', require('./auth.routes'))
+router.use('/quality', require('./auth.routes'))
+router.use('/profession', require('./auth.routes'))
+router.use('/user', require('./auth.routes'))
+
+module.exports = router
